66ca9beaf91b092e7867f8aec6531494dacb059a
Sarthak Goyal <112710237+sgoyalp1-bits@users.noreply.github.com>
2023-12-01 23:15:11 +0530

    (Backend) MongoDB and API Configurations

---
+++ backend/mongodb.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3000;
+
+const uri = process.env.DB_URL;
+const client = new MongoClient(uri);
+
+app.get('/getData/:table', async (req, res) => {
+    const { table } = req.params;
+  
+    try {
+      await client.connect();
+      const database = client.db('travel-app-data');
+      const collection = database.collection(table);
+  
+      const data = await collection.find({}).toArray();
+  
+      res.json(data);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    } finally {
+      await client.close();
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
